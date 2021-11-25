ef6c73a4785727b3b380a4addc23057b789f6289
ramkumar-1819 <rohitram1819@gmail.com>
2021-11-25 10:53:05 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React, { useRef } from 'react';
 import './App.css';
 
 function App() {
+  const rect=useRef();
+  let clicked=false;
+  let initialX=0;
+  let initialY=0;
+  let parentOffsetX;
+  let parentOffsetY;
+  const startDrawing=(e)=>{
+    console.log(e.offsetTop)
+    e.stopPropagation();
+    clicked=true;
+    rect.current.style.display='block';
+    rect.current.style.left=`${e.nativeEvent.offsetX}px`;
+    rect.current.style.top=`${e.nativeEvent.offsetY}px`;
+    parentOffsetX=e.nativeEvent.offsetX;
+    parentOffsetY=e.nativeEvent.offsetY;
+    initialX=e.pageX;
+    initialY=e.pageY;
+  }
+  const cursorMove=(e)=>{
+    if(clicked){
+      if(initialX<e.pageX && initialY<e.pageY){
+        rect.current.style.width=`${e.pageX-initialX}px`;
+        rect.current.style.height=`${e.pageY-initialY}px`;
+      }
+      else if(initialX>e.pageX && initialY<e.pageY){
+        const parent=document.getElementsByClassName('App')[0];
+        if(e.target===parent){
+          rect.current.style.left=`${e.nativeEvent.offsetX}px`;
+        }
+        else{
+          console.log('Move',e.nativeEvent.offsetX)
+          rect.current.style.left=`${parentOffsetX-e.nativeEvent.offsetX}px`;
+        }
+        rect.current.style.width=`${initialX-e.pageX}px`;
+        rect.current.style.height=`${e.pageY-initialY}px`;
+      }
+    }
+  }
+  const stopDrawing=(e)=>{
+    e.stopPropagation();
+    clicked=false;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" onMouseDown={startDrawing} onMouseUp={stopDrawing} onMouseMove={cursorMove}>
+      <div className='rect' ref={rect}></div>
     </div>
   );
 }
