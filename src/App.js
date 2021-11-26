02e450a10c11bbda32661e796fd2f1ed8fc1f559
ramkumar-1819 <rohitram1819@gmail.com>
2021-11-26 18:17:57 +0530

    East Drag Done

--- src/App.js
+++ src/App.js
@@ -3,64 +3,138 @@ import './App.css';
 
 function App() {
   const rect=useRef();
+  const east=useRef();
+  const west=useRef();
+
   let clicked=false;
+  let drawn=false;
   let left;
   let top; 
   let initialX;
   let initialY;
   let parent;
   let child;
+  let dimensions={};
+  let type='initial';
 
+  let eastDrag=false;
+  let westDrag=false;
+  let dragging;
+  let leftDrag;
+
+  //Initiate Drawing
   const startDrawing=(e)=>{
-    parent=e.target.getBoundingClientRect();
-    rect.current.style.display='block';
-    child=rect.current.getBoundingClientRect();
-    console.log(parent,child)
-    left=e.nativeEvent.offsetX;
-    top=e.nativeEvent.offsetY;
-    rect.current.style.top=`${top}px`;
-    rect.current.style.left=`${left}px`;
-    initialX=e.pageX;
-    initialY=e.pageY;
-    clicked=true;
-  }
-  const cursorMove=(e)=>{
-    
-    if(clicked){
-      if(initialX<e.pageX && initialY<e.pageY){
-        console.log('1')
-        rect.current.style.width=`${e.pageX-initialX}px`;
-        rect.current.style.height=`${e.pageY-initialY}px`;
-      }
-      else if(initialX>e.pageX && initialY<e.pageY){
-        console.log('2')
-        rect.current.style.left=`${e.pageX-parent.left}px`;
-        rect.current.style.width=`${initialX-e.pageX}px`;
-        rect.current.style.height=`${e.pageY-initialY}px`;
-      }
-      else if(initialX<e.pageX && initialY>e.pageY){
-        console.log('3')
-        rect.current.style.top=`${e.pageY-parent.top}px`;
-        rect.current.style.width=`${e.pageX-initialX}px`;
-        rect.current.style.height=`${initialY-e.pageY}px`;
-      }
-      else if(initialX>e.pageX && initialY>e.pageY){
-        console.log('4')
-        rect.current.style.top=`${e.pageY-parent.top}px`;
-        rect.current.style.left=`${e.pageX-parent.left}px`;
-        rect.current.style.width=`${initialX-e.pageX}px`;
-        rect.current.style.height=`${initialY-e.pageY}px`;
-      }
+    if(!drawn){
+      parent=e.target.getBoundingClientRect();
+      rect.current.style.display='block';
+      child=rect.current.getBoundingClientRect();
+      console.log(parent,child)
+      left=e.nativeEvent.offsetX;
+      top=e.nativeEvent.offsetY;
+      rect.current.style.top=`${top}px`;
+      rect.current.style.left=`${left}px`;
+      initialX=e.pageX;
+      initialY=e.pageY;
+      clicked=true;
     }
   }
+  //Continue Drawing.
+  const cursorMove=(e)=>{
+    switch(type){
+      case 'initial':
+        if(clicked){
+          if(initialX<e.pageX && initialY<e.pageY){
+            console.log('1')
+            rect.current.style.width=`${e.pageX-initialX}px`;
+            rect.current.style.height=`${e.pageY-initialY}px`;
+          }
+          else if(initialX>e.pageX && initialY<e.pageY){
+            console.log('2')
+            rect.current.style.left=`${e.pageX-parent.left}px`;
+            rect.current.style.width=`${initialX-e.pageX}px`;
+            rect.current.style.height=`${e.pageY-initialY}px`;
+          }
+          else if(initialX<e.pageX && initialY>e.pageY){
+            console.log('3')
+            rect.current.style.top=`${e.pageY-parent.top}px`;
+            rect.current.style.width=`${e.pageX-initialX}px`;
+            rect.current.style.height=`${initialY-e.pageY}px`;
+          }
+          else if(initialX>e.pageX && initialY>e.pageY){
+            console.log('4')
+            rect.current.style.top=`${e.pageY-parent.top}px`;
+            rect.current.style.left=`${e.pageX-parent.left}px`;
+            rect.current.style.width=`${initialX-e.pageX}px`;
+            rect.current.style.height=`${initialY-e.pageY}px`;
+          }
+        }
+        break;
+      case 'east':
+        if(eastDrag){
+          if(e.clientX<child['right']){
+            if(e.clientX<=child['left']){
+              rect.current.style.left=`${e.clientX-parent.left}px`
+              rect.current.style.width=`${child['left']-e.clientX}px`
+            }
+            else{
+              rect.current.style.width=`${child['width']-(child['right']- e.clientX)}px`;
+            }
+          }
+          else if(e.clientX>child['right']){
+            rect.current.style.width=`${(e.clientX-child['right'])+child['width']}px`;
+          }
+        }
+        break;
+      default:
+        break;
+    }
+  }
+  //Finish Drawing.
   const stopDrawing=(e)=>{
-    e.stopPropagation();
-    clicked=false;
+    switch(type){
+      case 'initial':
+        if(clicked){
+          drawn=true;
+          clicked=false;
+          leftDrag=true;
+          east.current.style.display='block';
+          west.current.style.display='block';
+        }
+        break;
+      case 'east':
+        if(eastDrag){
+          eastDrag=false;
+        }
+        break;
+      default:
+        break;
+    }
+    child=rect.current.getBoundingClientRect();
+    dimensions['x1']=rect.current.offsetLeft;
+    dimensions['y1']=rect.current.offsetTop;
+    dimensions['x2']=child.width + rect.current.offsetLeft;
+    dimensions['y2']=rect.current.offsetTop;
+    dimensions['x3']=rect.current.offsetLeft;
+    dimensions['y3']=child.height + rect.current.offsetTop;
+    dimensions['x4']=child.width + rect.current.offsetLeft;
+    dimensions['y4']=child.height + rect.current.offsetTop;
+    console.log(child,dimensions)
+  }
+  //East Side Dragging
+  const eastDraggingStart=(e)=>{
+    child=rect.current.getBoundingClientRect();
+    eastDrag=true;
+    type='east';
+  }
+  //West Side Dragging.
+  const westDraggingStart=(e)=>{
+
   }
   return (
     <div className="App" onMouseDown={startDrawing} onMouseUp={stopDrawing} onMouseMove={cursorMove}>
       <div className='rect' ref={rect}>
-        <span className='left'></span>
+        <span className='east' ref={east} onMouseDown={eastDraggingStart}></span>
+        <span className='west' ref={west} onMouseDown={westDraggingStart}></span>
       </div>
     </div>
   );
